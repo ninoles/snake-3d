4a57a2e601cfabacf8e7cc0f1ced05e0d2d48d3d
henrique <henrique@henrique-laptop>
2009-10-11 23:42:03 -0300

    Desenvolvimento do GameManagement 5%.

--- Codes/TesteMain.cpp
+++ Codes/TesteMain.cpp
@@ -7,10 +7,10 @@ using namespace base;
 
 int main() {
 
-	Frame *frame = new Frame(600, 400, 16, false, false);
+	Frame *frame = new Frame(600, 400, 16, true, false);
 
 	FrameButton button(frame->addButton(irr::core::rect<irr::s32>(10,240,110,240 + 32), 0, 0, L"New Window", L"Open new windows."));
-
+	FrameImage image(frame->addImage("/home/henrique/Imagens/grass2kl3.jpg", irr::core::position2d<irr::s32>(300,300), false, 0, 0, L"ASD"));
 
 	while(frame->frameRun())
 		if(frame->windowActive())
---
+++ Headers/Plataform/GameManagement.h
@@ -0,0 +1,28 @@
+/*
+ * GameManagement.h
+ *
+ *  Created on: 12/10/2009
+ *      Author: Henrique Jonas
+ *
+ */
+
+#ifndef GAMEMANAGEMENT_H_
+#define GAMEMANAGEMENT_H_
+
+#include <irrlicht.h>
+
+namespace plataform{
+
+	class GameManagement{
+
+		public:
+
+			static irr::video::ITexture* loadTexture(const irr::c8 *__filename);
+			static irr::gui::IGUIFont* loadFont(const irr::c8* __filename);
+
+
+	};
+};
+
+
+#endif /* GAMEMANAGEMENT_H_ */
